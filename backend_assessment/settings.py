6e435e9bddec3ca9ba7dfa4fc4a4c51646d5a1ef
Brian Tran <bkt5031@gmail.com>
2022-11-10 11:58:01 -0500

    hide secret key

--- backend_assessment/settings.py
+++ backend_assessment/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from .secret import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1evi57ty2nxda@9)p@=r(f-f#d5(0l&dbb=c=n^2bcghnev!vj'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
